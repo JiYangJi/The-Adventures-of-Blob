9f1e9134a2b0aee298e7eca2e78a5a72202ed93a
Umer Amjad <m.amjad.umer@gmail.com>
2018-07-06 19:16:22 -0400

    Refactored moving and jumping functions outside of FixedUpdate fn

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlobController : MonoBehaviour {
-    public enum direction { left, right, up, down};
-
+    public bool facingLeft = true;
     public float maxSpeed = 10;
-    public float jump = 30f;
-    public direction facing = direction.right;
+    public float jumpAmount = 30f;
     public bool stoppedUp = true;
     public int numJumps = 1;
     public int jumpCounter;
@@ -40,36 +38,49 @@ public class BlobController : MonoBehaviour {
         }
     }
 
+    void move(float amount) {
+        body.velocity = new Vector2(amount * maxSpeed, body.velocity.y);
+        checkFlip(amount);
+    }
+
+    void checkFlip(float amount) {
+        if ((amount > 0 && !facingLeft) || (amount < 0 && facingLeft))
+        {
+            FlipLeftRight();
+        }
+    }
+
+    void jump() {
+        body.velocity = new Vector2(body.velocity.x, jumpAmount);
+    }
+
     // FixedUpdate not necessarily the same as frame update
     void FixedUpdate()
     {
         float move_x = Input.GetAxis("Horizontal");
-        float move_y = Input.GetAxis("Vertical");
-        if (move_y == 0) {
+        bool pressedJump =  Input.GetButtonDown("Jump");
+
+        if (!Input.GetButtonUp("Jump"))
+        {
             stoppedUp = true;
         }
+        else
+        {
+            stoppedUp = false; 
+        } 
         bool grounded = isGrounded();
         if (grounded) {
             jumpCounter = numJumps;
         }
 
-        if ((grounded || jumpCounter > 0) && stoppedUp && move_y > 0.001)
+        if (!pressedJump)
         {
-            body.velocity = new Vector2(move_x * maxSpeed, jump);
-            stoppedUp = false;
-            jumpCounter--;
-        }
-        else {
-            body.velocity = new Vector2(move_x * maxSpeed, body.velocity.y);
+            move(move_x);
         }
 
-        if (move_x > 0 && facing == direction.left)
-        {
-            FlipLeftRight();
-        }
-        else if (move_x < 0 && facing == direction.right)
-        {
-            FlipLeftRight();
+        if (pressedJump && stoppedUp && (grounded || jumpCounter > 0)) {
+            jump();
+            jumpCounter--;
         }
     }
 
@@ -89,30 +100,10 @@ public class BlobController : MonoBehaviour {
         return false;
     }
 
-    //ground colliding
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-    }
-
     void FlipLeftRight()
     {
-        if (facing == direction.left)
-        {
-            facing = direction.right;
-        } 
-        else if (facing == direction.right)
-        {
-            facing = direction.left;
-        }
 
+        facingLeft = !facingLeft; 
         transform.localScale *= new Vector2(-1, 1);
     }
 
