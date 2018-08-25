6d7cc1e60cb8a3fb9e7e51e2dc5e33a62b7f1570
Umer Amjad <m.amjad.umer@gmail.com>
2018-08-25 00:29:50 -0400

    Removed debug logs

--- Assets/Scripts/WeaponAttack.cs
+++ Assets/Scripts/WeaponAttack.cs
@@ -8,7 +8,6 @@ public class WeaponAttack : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        Debug.Log("Reached start!");
         player = GameObject.Find("Player").GetComponent<Player>();
 
     }
@@ -19,12 +18,9 @@ public class WeaponAttack : MonoBehaviour {
 	}
 
     private void OnTriggerEnter2D(Collider2D collider) {
-        Debug.Log("Reached collider!");
         if (collider.tag == "Enemy") {
-            Debug.Log("Reached enemy!");
             Character toAttack = collider.GetComponentInParent<Character>();
             toAttack.attackCharacter(player.attack, toAttack.transform.position - this.transform.position, 120);
-            Debug.Log("This Position: " + transform.position.ToString());
         }
     }
 }
