2cb83ff389995054d898738afdd1847730cbe31b
Umer Amjad <m.amjad.umer@gmail.com>
2019-01-23 00:04:16 -0800

    Make camera snappier

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,7 +18,33 @@ public class CameraController : MonoBehaviour {
         //disregard player's z coordinate
         camera.z = transform.position.z;
         transform.position = camera;
-        float newSize = 5 + Mathf.Sqrt(Vector2.Distance(transform.position, player.transform.position));
-        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, newSize, 0.01f);
+        float dist = Mathf.Sqrt(Vector2.Distance(transform.position, player.transform.position));
+        float newSize = 5 + dist;
+        Debug.Log("Distance is: " + dist);
+        float interp = 0;
+        if (dist > 1) {
+            interp = 0.01f;
+        } else if (dist > 0.8) {
+            interp = 0.02f;
+        } else if (dist > 0.6) {
+            interp = 0.03f;
+        } else if (dist > 0.4) {
+            interp = 0.04f;
+        } else if (dist > 0.2) {
+            interp = 0.05f;
+        } else if (dist > 0.1) {
+            interp = 0.06f;
+        } else if (dist > 0.05) {
+            interp = 0.07f;
+        } else if (dist > 0.01) {
+            interp = 0.08f;
+        } else if (dist > 0.005) {
+            interp = 0.09f;
+        } else if (dist > 0.001) {
+            interp = 0.1f;
+        } else {
+            interp = 0.2f;
+        }
+        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, newSize, interp);
     }
 }
